fe26e702dde2001c83fc4359c550d99823e0eace
noviaretno <32433423+noviaretno@users.noreply.github.com>
2017-10-22 07:28:21 -0700

    selesai

--- soal4.cpp
+++ soal4.cpp
@@ -10,4 +10,14 @@ int main (){
 	cout << " luas 1 box parket    = "<<luas1box<<" m2"<<endl;
 	
 	double jumlahboxP, BiayaP, BiayaPG, TotalB;
+	
+	jumlahboxP = luasRumah/luas1box;
+	cout <<"jumlah box parket        = "<<jumlahboxP<<endl;
+	BiayaP = jumlahboxP*500;
+	cout<< " Biaya parket (Rp)       = "<<BiayaP<<endl;
+	BiayaPG = luasRumah*20;
+	cout<<" Biaya pasang (rp)        = " <<BiayaPG<<endl;
+	TotalB = BiayaP+BiayaPG;
+	cout<< " total Biaya (rp)        =" <<TotalB<<endl;
+	
 }
