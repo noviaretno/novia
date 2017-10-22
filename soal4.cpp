b09198315bc5cd3c7a351b57f2fe1a32c5f8c376
noviaretno <32433423+noviaretno@users.noreply.github.com>
2017-10-22 06:55:54 -0700

    output hasil luas 1 box

--- soal4.cpp
+++ soal4.cpp
@@ -5,4 +5,7 @@ int main (){
 	double luasRumah = 42;
 	double lantaipkt = 193*1215;
 	double luas1box = lantaipkt*8/1000000;
+	
+	cout << " luas rumah           = "<<luasRumah<<" m2"<<endl;
+	cout << " luas 1 box parket    = "<<luas1box<<" m2"<<endl;
 }
