a5a99ee843658d5e14ecb276188aa3c55a9d3a05
noviaretno <32433423+noviaretno@users.noreply.github.com>
2017-10-22 05:24:52 -0700

    perbaikan

--- tugas2.cpp
+++ tugas2.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 int main (){
-	nilai=nilai+1;
 	int nilai;
-	int 1nilai;
-	
-	cout << nilai;
-	cout << 1nilai;
+	int Nilai = 10;
+	nilai = Nilai+1;
+	cout <<"jumlah = "<<nilai;
 }
